4540a39186326316aa3b348a874cdc083c9e6b9f
sreeUNIFI <sreenivasa.nalamalpu@stud.unifi.it>
2021-03-20 21:54:20 +0100

    Created repository layer for recipe and UnitOFMeasure

---
+++ src/main/java/com/example/atat/repository/Recipe.java
@@ -0,0 +1,7 @@
+package com.example.atat.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Recipe extends CrudRepository<Recipe, Long> {
+
+}
---
+++ src/main/java/com/example/atat/repository/UnitOfMeasure.java
@@ -0,0 +1,6 @@
+package com.example.atat.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasure extends CrudRepository<UnitOfMeasure, Long> {
+}
